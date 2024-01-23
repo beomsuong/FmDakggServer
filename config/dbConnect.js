cea52a62765484f8b8ac8afdabc618034249261c
beomsuong <beomsuong0406@gmail.com>
2024-01-23 23:30:33 +0900

    db test

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,12 +1,38 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const userInfoSchema = new mongoose.Schema({
+  _id: String,//userNum
+  gameList: [{
+    gameId: String, 
+    timestamp: Date 
+  }],
+});
+
+const gameInfoSchema = new mongoose.Schema({
+  _id: String,//gameNum
+  id: Number,
+});
+
+
+const UserInfo = mongoose.model('UserInfo', userInfoSchema, 'userInfo');
+const gameInfo = mongoose.model('gameInfo', gameInfoSchema, 'gameInfo');
+require("dotenv").config();
+
 const dbConnect = async () => {
   try {
-    const connect = await mongoose.connect(process.env.DB_CONNECT);
+    await mongoose.connect(process.env.DB_CONNECT, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log("DB connected");
+
+    const newUser = new UserInfo({_id:'123', id: 100 });
+    await newUser.save();
+    console.log('성공');
+
   } catch (err) {
-    console.log(err);
+    console.error('실패:', err);
   }
 };
 
