d843ed4f2b763587808d92e3694366186eb089d5
beomsuong <beomsuong0406@gmail.com>
2024-01-27 23:48:52 +0900

    lint 적용중

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,9 +1,7 @@
-const mongoose = require("mongoose");
-const userInfoSchema = require("../models/userInfoModel");
-const gameInfoSchema = require("../models/gameInfoModel");
-require("dotenv").config();
-
-
+const mongoose = require('mongoose');
+const userInfoSchema = require('../models/userInfoModel');
+const gameInfoSchema = require('../models/gameInfoModel');
+require('dotenv').config();
 
 // const UserInfo = mongoose.model('userInfo', userInfoSchema, 'userInfo');
 // const gameInfo = mongoose.model('gameInfo', gameInfoSchema, 'gameInfo');
@@ -12,20 +10,17 @@ const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.DB_CONNECT, {
     });
-    console.log("DB connected");
+    console.log('DB connected');
 
     // const gameInfo = mongoose.model('gameInfo', gameInfoSchema, 'gameInfo');
     // const UserInfo = mongoose.model('userInfo', userInfoSchema, 'userInfo');
-    
+
     // const newUser = new gameInfo({ _id: 112300,id: 112300 });
     // await newUser.save();
 
- 
     console.log('DB 연결 성공');
-
   } catch (err) {
     console.error('DB 연결 실패:', err);
   }
 };
-
 module.exports = dbConnect;
